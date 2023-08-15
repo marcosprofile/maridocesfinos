ec83019a3e5fec9d5aba764851e63a849d121fe8
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-08-15 12:05:06 -0300

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/Theme';
 import Home from './pages/Home';
@@ -7,11 +8,22 @@ import Header from './components/Header';
 import { MainBody } from './styles/Global.styled';
 
 function App() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+  
+    React.useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Header />
         <MainBody>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/catalogo" element={<List />} />
