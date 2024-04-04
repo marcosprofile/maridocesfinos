ce37d7314a621adb53739a0d6bdf3e354e8f0beb
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-04-03 22:05:53 -0300

    feat: update colors product name

--- src/layouts/List.jsx
+++ src/layouts/List.jsx
@@ -12,8 +12,8 @@ const List = ({ data }) => {
     <>
       <Image width="100%" height="280px" src={data.product_img} respImgCard draggable="false" />
       <FlexContainer  direction="column" padding=".75rem">
-        <Heading as="h5" size="h5" weight="bold" grey>{ data.product_name }</Heading>
-        <Heading as="p" size="p" grey>{ data.product_desc }</Heading>
+        <Heading as="h5" size="h5" weight="bold" color="primary">{ data.product_name }</Heading>
+        <Heading as="p" size="p" color="grey">{ data.product_desc }</Heading>
         <FlexContainer marginTop="1rem" marginBottom=".5rem" width="100%" justify="center">
           <Price>{ data.price }</Price>
         </FlexContainer>
